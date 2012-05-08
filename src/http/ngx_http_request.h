eb3454e68c57159de6209b140cf9102d6f6c6fa9
Simon Liu <simohayha.bobo@gmail.com>
2012-05-08 19:34:54 +0800

    modify nginx http start code.

--- src/http/ngx_http_request.h
+++ src/http/ngx_http_request.h
@@ -101,7 +101,7 @@
 /* The special code to close connection without any response */
 #define NGX_HTTP_CLOSE                     444
 
-#define NGX_HTTP_NGINX_CODES               494
+#define NGX_HTTP_NGINX_CODES               495
 
 #define NGX_HTTPS_CERT_ERROR               495
 #define NGX_HTTPS_NO_CERT                  496
